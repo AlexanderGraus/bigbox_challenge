4dbeaeb78bef441bfa2e4d4328087ff8e5cfb11b
ale613 <alegraus31@gmail.com>
2021-05-23 01:55:18 -0300

    lista las activities de un box especifico

--- bigbox/urls.py
+++ bigbox/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('box/', views.box_list, name= 'box_list'),
-    path('box/<int:pk>',views.box_detail, name= 'box_detail'),
+    path('box/<int:pk>/',views.box_detail, name= 'box_detail'),
+    path('box/<int:pk>/activity/',views.activity_list_by_box, name= 'activity_list_by_box'),
 ]
--- bigbox/views.py
+++ bigbox/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Box, Activity, Category, Reason
-from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
 # Create your views here.
 
 def box_list(request):
@@ -11,3 +11,14 @@ def box_detail(request,pk):
     box = Box.objects.get(pk=pk)
     activities = box.activities.all()[:5] # guarda las primeras 5 activities
     return render(request,'box_detail.html',{"box":box,"activities":activities})
+
+def activity_list_by_box(request,pk):
+    box_name = Box.objects.get(pk=pk).name
+    activities = Activity.objects.filter(box__pk=pk)
+    print(activities[0].name)
+    print(box_name)
+
+    paginator = Paginator(activities, 20) # muestra 20 resultados por pagina
+    page = request.GET.get('page')
+    activities = paginator.get_page(page)
+    return render(request,'activity_list.html',{"activities":activities, "box_name":box_name})
